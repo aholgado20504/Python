0e4db5d3d0b4b64e372a43df154e1671c9af40a8
aholgado20504 <aholgado20504@iesjoanramis.org>
2025-11-12 20:32:08 +0100

    Exercici 11

---
+++ Exercici_1.py
@@ -0,0 +1,4 @@
+# Llegir un nom i cognom per consola i imprimir-los en ordre invers.
+nom = input("introdueix el teu nom: ")
+cognom = input("introdueix el teu cognom: ")
+print("Hola, " + cognom + ", " + nom)
