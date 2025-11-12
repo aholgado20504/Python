ebe5122b46980250d8d035cce77a8deee790a8fb
aholgado20504 <aholgado20504@iesjoanramis.org>
2025-11-12 13:39:29 +0100

    Ex1.c

---
+++ prova 2.py
@@ -0,0 +1,16 @@
+a = float(input("Escriu el primer nombre: "))
+b = float(input("Escriu el segon nombre: "))
+c = float(input("Escriu el terçer nombre: "))
+d = a + b + c
+print("La suma {} + {} + {} és {}".format(a, b, c, d))
+if d > 20:
+    print("La suma és major que 20, ja que és {}".format(d))
+else:
+    print("La suma no és major que 20, ja que és {}".format(d))
+
+d = a * b * c
+print("La multiplicació {} * {} * {} és {}".format(a, b, c, d))
+if d > 100:
+    print("La multiplicació és major que 100, ja que és {}".format(d))
+else:
+    print("La multiplicació no és major que 100, ja que és {}".format(d))
