09ffcc2d305ec8293f2815cbfde7eb31dfb0624c
aholgado20504 <aholgado20504@iesjoanramis.org>
2025-11-12 12:57:43 +0100

    prova 1

---
+++ prova1.py
@@ -0,0 +1,6 @@
+a = int(input("Escriu el primer nombre: "))
+b = int(input("Escriu el segon nombre: "))
+c = a + b
+print("El resultat de la suma {} + {} és {}".format(a, b, c))
+c = a * b
+print("El resultat de la multiplicació {} * {} és {}".format(a, b, c))
