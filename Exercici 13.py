5e37a14a4f154b6a2cebdf22d95c51b7c336dd63
aholgado20504 <aholgado20504@iesjoanramis.org>
2025-11-13 11:27:44 +0100

    Exercici 14. Joan ja em pots donar bona nota

--- Exercici 13.py
+++ Exercici 13.py
@@ -1,12 +1,20 @@
-# calculadora que permet utilitzar totes les operacions en números enters i de punt flotant.
-numero = input("Vols treballar amb nombres enters o de punt flotant? (e/f): ").lower()
-if numero == 'e':
-    num1 = int(input("Introdueix el primer nombre enter: "))
-    num2 = int(input("Introdueix el segon nombre enter: "))
-elif numero == 'f':
-    num1 = float(input("Introdueix el primer nombre de punt flotant: "))
-    num2 = float(input("Introdueix el segon nombre de punt flotant: "))
-operacio = input("Introdueix l'operació que vols realitzar (+, -, *, /): ")
+# calculadora que permet utilitzar totes les operacions en números enters i de punt flotant. funcionalitat que perment treballar amb canvis de base (binari, octal, decimal i hexadecimal).
+base = int(input("Introdueix la base numèrica (2-binari, 8-octal, 10-decimal, 16-hexadecimal): "))
+num1 = input("Introdueix el primer número: ")
+num2 = input("Introdueix el segon número: ")
+if base == 2:
+    num1 = int(num1, 2)
+    num2 = int(num2, 2)
+elif base == 8:
+    num1 = int(num1, 8)
+    num2 = int(num2, 8)
+elif base == 16:
+    num1 = int(num1, 16)
+    num2 = int(num2, 16)
+else:
+    num1 = float(num1)
+    num2 = float(num2)
+operacio = input("Introdueix l'operació (+, -, *, /, //, %, **): ")
 if operacio == '+':
     resultat = num1 + num2
 elif operacio == '-':
@@ -14,10 +22,24 @@ elif operacio == '-':
 elif operacio == '*':
     resultat = num1 * num2
 elif operacio == '/':
-    if num2 != 0:
-        resultat = num1 / num2
-    else:
-        resultat = "Error: Divisió per zero no permesa."
+    resultat = num1 / num2
+elif operacio == '//':
+    resultat = num1 // num2
+elif operacio == '%':
+    resultat = num1 % num2
+elif operacio == '**':
+    resultat = num1 ** num2
 else:
-    resultat = "Error: Operació no vàlida."
-print("El resultat és:", resultat)
+    resultat = "Operació no vàlida"
+if base == 2:
+    print("Resultat en binari:", bin(int(resultat)))
+elif base == 8:
+    print("Resultat en octal:", oct(int(resultat)))
+elif base == 16:
+    print("Resultat en hexadecimal:", hex(int(resultat)))
+else:
+    print("Resultat en decimal:", resultat)
+
+# Me cago en la puta
+
+
