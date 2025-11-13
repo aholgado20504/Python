3e587774c3a952e111d36903cb3215157cb436c1
aholgado20504 <aholgado20504@iesjoanramis.org>
2025-11-13 11:49:21 +0100

    Exercici 12

--- Exercici 12b.py
+++ Exercici 12b.py
@@ -1,8 +1,12 @@
 # Llegir una edat i imprimir si es major d'edat, no es major d'edat o exactament 18 anys.
+def majoredat(edat):
+    if edat > 18:
+        return "Ets major d'edat."
+    elif edat < 18:
+        return "No ets major d'edat."
+    else:
+        return "Tens exactament 18 anys."
 edat = int(input("Introdueix la teva edat: "))
-if edat > 18:
-    print("Ets major d'edat.")
-elif edat < 18:
-    print("Ets menor d'edat.")
-else:
-    print("Tens exactament 18 anys.")
+majoredat(edat)
+edat = int(input("Introdueix la teva edat: "))
+majoredat(edat)
