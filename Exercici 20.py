23efa445f68f79e989376d3c37b13cd4b4309662
aholgado20504 <aholgado20504@iesjoanramis.org>
2025-11-19 12:43:19 +0100

    Exercici 20

---
+++ Exercici 20.py
@@ -0,0 +1,12 @@
+# Definir una funció invertir() que calculi la inversa d’una cadena. Ex: invertir(“Soc del Ramis”) hauria de tornar la cadena “simaR led coS”.
+def invertir(cadena):
+    cadena_invertida = ""
+    for caracter in cadena:
+        cadena_invertida = caracter + cadena_invertida
+    return cadena_invertida
+
+# Proves de la funció invertir
+print(invertir("Soc del Ramis"))    # Ha de retornar "simaR led coS"
+print(invertir("Python"))   # Ha de retornar "nohtyP"
+print(invertir("12345"))    # Ha de retornar "54321"
+print(invertir("Me ha dolido el ano"))  # Ha de retornar "ona le odilod ah eM"
