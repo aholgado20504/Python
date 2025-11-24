4992600589cd5b845e7c98ba589de665e0aaf093
aholgado20504 <aholgado20504@iesjoanramis.org>
2025-11-24 09:52:44 +0100

    Exercici 18

--- Exercici 18.py
+++ Exercici 18.py
@@ -1,13 +1,9 @@
 def es_vocal(caracter):
-    vocals = "aeiouAEIOUàáèéìíóòúù"
+    vocals = "aeiouAEIOUàáèéìíóòúùÀÁÉÈÌÍÓÒÚÙ"
     if caracter in vocals and len(caracter) == 1:
         return True
     else:
         return False
 # Proves de la funció es_vocal
-print(es_vocal("a"))  # Ha de retornar True
-print(es_vocal("E"))  # Ha de retornar True
-print(es_vocal("b"))  # Ha de retornar False
-print(es_vocal("O"))  # Ha de retornar True
-print(es_vocal("z"))  # Ha de retornar False
-print(es_vocal("ae")) # Ha de retornar False (perque n'hi han més d'un caràcter)
+caracter = input("Escriu una lletra i esbrinaré si és vocal o no: ")
+print(es_vocal(caracter))
