779f24746b5dfcb3ad3be4292709094c47a618fb
aholgado20504 <aholgado20504@iesjoanramis.org>
2025-11-24 09:44:01 +0100

    ex17

--- Exercici 17.py
+++ Exercici 17.py
@@ -1,11 +1,7 @@
-def longitud(element):
-    contador = 0
-    for _ in element:
-        contador += 1
-    return contador
-var = longitud([1, 2, 3, 4, 5])
-print("La longitud de la llista és:", var)
-var = longitud([1, 3, 5 , 7, 9, 11, 13])
-print("La longitud de la llista és:", var) 
-var = longitud("Adeu, món!")
-print("La longitud de la cadena és:", var)
+def ex17(a):
+    return len(a)
+
+a=[1,3,4,5, "Juan Alberto III", [3,4],6,7,[1,[3,4,2],1],4]
+print(ex17(a))
+b = "Som en Joan Ramis, i tú què tal estàs?"
+print(ex17(b))
