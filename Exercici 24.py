65c702cd27656b20a117e9426f450d276dcc52b6
aholgado20504 <aholgado20504@iesjoanramis.org>
2025-11-26 13:34:06 +0100

    Exercici 24

--- Exercici 24.py
+++ Exercici 24.py
@@ -1,8 +1,5 @@
-# Definir una funció crear_punts() que agafi una llista de números i que pinti per pantalla tants punts com el valor de cada número de la llista. Entre els elements de la llista, hi ha d’haver un salt de línia. Ex: crear_punts([2,3,4]) mostri per pantalla el següent:
 num = int(input("Introdueix quants punts vols a la llista: "))
 def crear_punts(llista):
     for num in llista:
         print('.' * num)
 crear_punts(list(range(1, num + 1)))
-
-
